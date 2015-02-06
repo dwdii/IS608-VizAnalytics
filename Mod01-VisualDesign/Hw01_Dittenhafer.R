d26d85e05819f7d7b155716bd836da4f9f988a74
Daniel Dittenhafer <daniel@dittenhafer.net>
2015-02-06 13:56:25 -0500

    RC1

--- Mod01-VisualDesign/Hw01_Dittenhafer.R
+++ Mod01-VisualDesign/Hw01_Dittenhafer.R
@@ -56,23 +56,35 @@ completeStateData <- stateData[ok,]
 
 # Aggregate by industry
 employmentByIndustry <- plyr::ddply(completeStateData, c("State", "Industry"), plyr::summarize, mean = mean(Employees), sd = sd(Employees), .inform=TRUE)
+
+# Put a floor on the minimum (-1 stdev) at zero
+employmentByIndustry$min <- employmentByIndustry$mean - employmentByIndustry$sd
+employmentByIndustry$min[employmentByIndustry$min < 0] = 0
+
 #employmentByIndustry <- plyr::arrange(stateData, freq)
 #countByState$State <- factor(countByState$State, levels=unique(as.character(countByState$State)) )
+completeStateDataMSD <- plyr::join(completeStateData, employmentByIndustry, by=c("State", "Industry"))
 
-print(employmentByIndustry)
+
+head(completeStateDataMSD)
+
+#nonOutliers <- dplyr::filter(completeStateDataMSD, Employees < mean + (2 * sd))
+
 
 # Visualize 
-g2 <- ggplot(data=completeStateData, aes(x=Industry, y=Employees)) 
+g2 <- ggplot(data=employmentByIndustry, aes(x=Industry, y=mean, color=mean)) 
 g2 <- g2 + theme(axis.ticks=element_blank(), 
-                 axis.text.x=element_text(angle=45,hjust=1),
+                 axis.text.x=element_text(angle=60,hjust=1),
                  panel.border = element_rect(color="gray", fill=NA),
                  panel.background=element_rect(fill="#FBFBFB"),
                  panel.grid.major.y=element_line(color="white", size=0.5),
                  panel.grid.major.x=element_line(color="white", size=0.5))
 #g2 <- g2 + stat_smooth(method=lm,aes(group=1))
-g2 <- g2 + geom_violin()
+#g2 <- g2 + geom_smooth(aes(group=1))
+#g2 <- g2 + geom_boxplot(outlier.shape=NA)
 #g2 <- g2 + geom_bar(stat="identity") 
-g2 <- g2 + geom_point(position="jitter") 
+#g2 <- g2 + geom_point(position="jitter") 
+g2 <- g2 + geom_pointrange(aes(ymin=min, ymax=mean + sd))
 #g2 <- g2 + scale_x_discrete(breaks=waiver())
 #g2 <- g2 + scale_y_continuous(breaks=c(seq(0,800,by=200)), minor_breaks=NULL)
 #g2 <- g2 + coord_flip()
@@ -81,3 +93,38 @@ g2
 
 # Save the chart to a file.
 ggsave("Figure2.png", width = 4.8, height=6.4, dpi=300)
+
+# http://stackoverflow.com/questions/5677885/ignore-outliers-in-ggplot2-boxplot
+
+#######
+# Q3 = Most Revenue per Employee by Industry
+#
+ok <- complete.cases(inc5k)
+completeData <- stateData[ok,]
+avgEmpRevByInd <- plyr::ddply(completeData, c("Industry"), plyr::summarize, meanEmployees = mean(Employees), meanRevenue = mean(Revenue), .inform=TRUE)
+avgEmpRevByInd$meanRevPerEmp <- avgEmpRevByInd$meanRevenue / avgEmpRevByInd$meanEmployees
+avgEmpRevByInd <- plyr::arrange(avgEmpRevByInd, meanRevPerEmp, decreasing=TRUE)
+avgEmpRevByInd$Industry <- factor(avgEmpRevByInd$Industry, levels=unique(as.character(avgEmpRevByInd$Industry)) )
+
+# Visualize 
+g3 <- ggplot(data=avgEmpRevByInd, aes(x=Industry, y=meanRevPerEmp)) 
+g3 <- g3 + theme(axis.ticks=element_blank(), 
+                 axis.text.x=element_text(angle=60,hjust=1),
+                 panel.border = element_rect(color="gray", fill=NA),
+                 panel.background=element_rect(fill="#FBFBFB"),
+                 panel.grid.major.y=element_line(color="white", size=0.5),
+                 panel.grid.major.x=element_line(color="white", size=0.5))
+#g2 <- g2 + stat_smooth(method=lm,aes(group=1))
+#g2 <- g2 + geom_smooth(aes(group=1))
+#g2 <- g2 + geom_boxplot(outlier.shape=NA)
+#g2 <- g2 + geom_bar(stat="identity") 
+g3 <- g3 + geom_point(position="jitter") 
+#g2 <- g2 + geom_pointrange(aes(ymin=min, ymax=mean + sd))
+#g2 <- g2 + scale_x_discrete(breaks=waiver())
+#g2 <- g2 + scale_y_continuous(breaks=c(seq(0,800,by=200)), minor_breaks=NULL)
+#g2 <- g2 + coord_flip()
+g3 <- g3 + labs(title=sprintf("Revenue per Employee By Industry (Inc 5000)"), x="", y="USD") 
+g3 
+
+# Save the chart to a file.
+ggsave("Figure3.png", width = 4.8, height=6.4, dpi=300)
